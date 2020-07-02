676d83e2a426431ee73741bd732a641029b5b6fc
Dalo <dali.molochnik@gmail.com>
2020-07-02 14:32:17 -0300

    Posting in mongoDB

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ var WishList = require('./model/wishlist');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.post('/product', function(request, response){
+  var product = new Product();
+  product.title = request.body.title;
+  product.price = request.body.price;
+  product.save(function(err, savedDocument){
+    if (err) {
+      response.status(500).send({error:"Product couldn't be saved"});
+    } else {
+      response.status(200).send(savedDocument);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log('Swag shop API up and running on port 3000...')
 });
